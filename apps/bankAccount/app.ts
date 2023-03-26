c8511b26c06536e28cfdd7af91085310b9a60c9b
Joan <jokrastanov@gmail.com>
2023-03-26 14:14:31 +0200

    bank account service folder structure added

---
+++ apps/bankAccount/app.ts
@@ -0,0 +1,25 @@
+import dotenv from "dotenv";
+import cors from "cors";
+import bodyParser from "body-parser";
+import express, { Express } from "express";
+
+dotenv.config();
+const app: Express = express();
+const PORT: String | number = process.env.PORT || 6000;
+
+// * CORS Policy
+app.use(
+  cors({
+    origin: "*",
+  })
+);
+
+
+app.use(bodyParser.urlencoded({ extended: false }));
+app.use(bodyParser.json());
+
+// * App Routes
+
+app.listen(PORT, async () => {
+  console.log("Server is running at port:", PORT);
+});
